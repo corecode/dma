fa8d8158f58c6b0b4cd2e91c5023c6ee05aad39c
Peter Pentchev <roam@ringlet.net>
2009-03-06 13:32:44 +0000

    Initial import of a crude attempt at a Debian package of dma, the DragonFly Mail Agent.

---
+++ dma.default.ex
@@ -0,0 +1,10 @@
+# Defaults for dma initscript
+# sourced by /etc/init.d/dma
+# installed at /etc/default/dma by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
