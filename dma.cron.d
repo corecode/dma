e2a1525279f1b060f7b508c68ed1bca4c74a3d9d
Peter Pentchev <roam@ringlet.net>
2010-06-15 11:01:57 +0000

    Invoke dma-migrate if present before flushing the queue.

--- dma.cron.d
+++ dma.cron.d
@@ -1,3 +1,3 @@
 # Flush the dma mail transfer agent's queue every five minutes.
 #
-*/5 *	* * *	root	[ -x /usr/sbin/dma ] && /usr/sbin/dma -q1
+*/5 *	* * *	root	[ -x /usr/sbin/dma-migrate ] && /usr/sbin/dma-migrate; [ -x /usr/sbin/dma ] && /usr/sbin/dma -q1
