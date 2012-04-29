58cb9f257d7f643dfbfedee186aee2fdfbca1a4a
Loganaden Velvindron <logan@leaf.dragonflybsd.org>
2012-04-29 17:08:43 +0400

    Drop privileges permanently

--- dma.c
+++ dma.c
@@ -427,7 +427,7 @@ main(int argc, char **argv)
 		if (pw == NULL)
 			err(1, "cannot drop root privileges");
 
-		if (setuid(pw->pw_uid) != 0)
+		if (setresuid(pw->pw_uid, pw->pw_uid, pw->pw_uid) != 0)
 			err(1, "cannot drop root privileges");
 
 		if (geteuid() == 0 || getuid() == 0)
