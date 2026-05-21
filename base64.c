7f50195e8c0c16b23901b50fe8a9c94968eada01
Tuukka Pasanen <tuukka.pasanen@ilmi.fi>
2026-05-21 11:03:00 +0300

    SPDX-License-Identifier: Add BSD-3-Clause identifiers
    Add SPDX identifiers BSD-3-Clause to most of c-source and
    header files

--- base64.c
+++ base64.c
@@ -1,4 +1,6 @@
 /*
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
  * Copyright (c) 1995-2001 Kungliga Tekniska Högskolan
  * (Royal Institute of Technology, Stockholm, Sweden).
  * All rights reserved.
--- dma.c
+++ dma.c
@@ -1,4 +1,6 @@
 /*
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
  * Copyright (c) 2008-2014, Simon Schubert <2@0x2c.org>.
  * Copyright (c) 2008 The DragonFly Project.  All rights reserved.
  *
